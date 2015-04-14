6170be60bcaf4bccfb158a793f5f93434da3af31
Patrick Best <patrickbest@patrickbest.com>
2015-04-14 18:05:14 -0500

    init from Master Theme

---
+++ 404.php
@@ -0,0 +1,18 @@
+<?php
+get_header();
+?>
+
+<!-- main content -->
+
+<section id="main-content" class="container">
+  <div class="row">
+    <div class="col col6">
+      <h2>Not found</h2>
+      <p>:(</p>
+    </div>
+<!-- end main-content -->
+</section>
+
+<?php
+get_footer();
+?>
---
+++ README.md
@@ -0,0 +1,9 @@
+# Chris Brooks punto Net
+
+### CB.NET
+
+## MASTER THEME
+
+#### chrisbrooks.net
+
+# chrisbrooks-net
