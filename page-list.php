b2a8a1786275670f7ef8cc3f7cd34bf6b4aaf30c
Patrick Best <patrickbest@patrickbest.com>
2015-05-14 13:59:26 -0500

    allow line breaking of titles in list view

--- page-list.php
+++ page-list.php
@@ -30,14 +30,12 @@ if ($posts) {
 if (!empty($meta['_igv_fig'][0])) {
   echo '<span class="list-fig">fig.' . $meta['_igv_fig'][0] . '</span>';
 }
-echo '<h4 class="list-title">';
 echo '<span class="font-italic">';
 the_title();
 echo '</span>';
 if (!empty($meta['_igv_year'][0])) {
   echo ', ' . $meta['_igv_year'][0];
 }
-echo '</h4>';
 if (!empty($meta['_igv_gallery_length'][0]) && $meta['_igv_gallery_length'][0] > 1) {
   echo '<div class="list-series-count">series of ' . $meta['_igv_gallery_length'][0] . ' images</div>';
 } else {
